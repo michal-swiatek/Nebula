d5751937115c85279456f601e0f68beb4f96fd98
michal-swiatek <swiatek.michal006@gmail.com>
2023-10-31 12:43:49 +0100

    Project setup

---
+++ Sandbox/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by michal-swiatek on 31.10.2023.
+// Github: https://github.com/michal-swiatek
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!\n";
+
+    return 0;
+}

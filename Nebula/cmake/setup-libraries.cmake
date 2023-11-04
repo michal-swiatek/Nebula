d3c90a9b8d7fb975d370f6c549ad0cbbdb1d5614
michal-swiatek <swiatek.michal006@gmail.com>
2023-11-04 04:41:30 +0100

    Added spdlog

--- Nebula/cmake/setup-libraries.cmake
+++ Nebula/cmake/setup-libraries.cmake
@@ -1,3 +1,22 @@
 set(OPENGL_MAJOR_VERSION 4)
 set(OPENGL_MINOR_VERSION 6)
 configure_file(include/OpenGLConfiguration.h.in include/OpenGLConfiguration.h @ONLY)
+
+set(CMAKE_PREFIX_PATH ${CMAKE_CURRENT_SOURCE_DIR}/3rd-party)
+
+set(BUILD_TYPE ${CMAKE_BUILD_TYPE})
+if (${BUILD_LIBRARIES_RELEASE})
+    set(CMAKE_BUILD_TYPE "Release")
+endif()
+message("Building 3rd-party libraries in ${CMAKE_BUILD_TYPE} mode.")
+
+# spdlog
+set(SPDLOG_USE_STD_FORMAT ON CACHE BOOL "Use std::format")
+set(SPDLOG_ENABLE_PCH ON CACHE BOOL "Use precompiled headers")
+
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/3rd-party/spdlog)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/3rd-party/spdlog/include)
+set(SPD_LOG ${spdlog})
+
+# Restore project build type
+set(CMAKE_BUILD_TYPE ${BUILD_TYPE})

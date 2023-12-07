ca168843f1e9b063772c3d5bc55d689f5edc78e3
michal-swiatek <swiatek.michal006@gmail.com>
2023-12-07 20:17:02 +0100

    Improve assert error messages with file and line info
    The macro NB_INTERNAL_ASSERT_WITH_MSG in the Assert.h file has been modified to include the filename and line number in the output message when an assertion fails. This facelift enhances debugging by pinpointing exactly where an error occurred and provides more context instantly.

--- Nebula/include/core/Assert.h
+++ Nebula/include/core/Assert.h
@@ -13,8 +13,8 @@
 
 #ifdef NB_ENABLE_ASSERTS
     #define NB_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { NB##type##ERROR(msg, __VA_ARGS__); NB_DEBUGBREAK(); } }
-    #define NB_INTERNAL_ASSERT_WITH_MSG(type, check, ...) NB_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-    #define NB_INTERNAL_ASSERT_NO_MSG(type, check) NB_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", NB_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
+    #define NB_INTERNAL_ASSERT_WITH_MSG(type, check, msg) NB_INTERNAL_ASSERT_IMPL(type, check, "{1}:{2}: {0}", msg, std::filesystem::path(__FILE__).string(), __LINE__)
+    #define NB_INTERNAL_ASSERT_NO_MSG(type, check) NB_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", NB_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).string(), __LINE__)
 
     #define NB_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
     #define NB_INTERNAL_ASSERT_GET_MACRO(...) NB_EXPAND_MACRO( NB_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, NB_INTERNAL_ASSERT_WITH_MSG, NB_INTERNAL_ASSERT_NO_MSG) )
